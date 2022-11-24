ea1522b3d85c639d94ed1edcb029a4b28e450109
cernobikinaje <jekaterina23cher@gmail.com>
2022-11-24 20:43:48 +0200

    [master]: Project initial upload

--- src/test/java/TestChrome.java
+++ src/test/java/TestChrome.java
@@ -10,8 +10,7 @@ public class TestChrome {
 
     ChromeDriver driver = new ChromeDriver();
 
-    public static String GOOGLE_URL = "https://www.google.lv/?hl=en";
-
+    public static final String GOOGLE_URL = "https://www.google.lv/?hl=en";
 
     @AfterMethod
     public void tearDown() {
@@ -31,6 +30,19 @@ public class TestChrome {
         searchField.sendKeys(Keys.ENTER);
 
         Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
+    }
 
+    @Test
+    public void chromeTest2() {
+        driver.get(GOOGLE_URL);
+
+        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
+        acceptButton.click();
+
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("acodemy");
+        searchField.sendKeys(Keys.ENTER);
+
+//        Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
     }
 }
