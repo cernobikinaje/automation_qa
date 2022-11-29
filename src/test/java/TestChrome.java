3f1d76fd5fae823e9737bf095b3ce583d4a31e19
cernobikinaje <jekaterina23cher@gmail.com>
2022-11-29 21:15:16 +0200

    [master]: Update TestChrome.java

--- src/test/java/TestChrome.java
+++ src/test/java/TestChrome.java
@@ -1,55 +1,55 @@
-import org.checkerframework.checker.units.qual.C;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-public class TestChrome {
-
-    ChromeDriver driver = new ChromeDriver();
-
-    public static final String GOOGLE_URL = "https://www.google.lv/?hl=en";
-
-    @BeforeMethod
-    public void before() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.close();
-        driver.quit();
-    }
-
-    @Test
-    public void chromeTest() {
-        driver.get(GOOGLE_URL);
-
-        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
-        acceptButton.click();
-
-        WebElement searchField = driver.findElement(By.name("q"));
-        searchField.sendKeys("acodemy");
-        searchField.sendKeys(Keys.ENTER);
-
-        Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
-    }
-
-    @Test(enabled = false)
-    public void chromeTest2() {
-        driver.get(GOOGLE_URL);
-
-        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
-        acceptButton.click();
-
-        WebElement searchField = driver.findElement(By.name("q"));
-        searchField.sendKeys("acodemy");
-        searchField.sendKeys(Keys.ENTER);
-
+//import org.checkerframework.checker.units.qual.C;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.Keys;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.Assert;
+//import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+//
+//public class TestChrome {
+//
+//    ChromeDriver driver = new ChromeDriver();
+//
+//    public static final String GOOGLE_URL = "https://www.google.lv/?hl=en";
+//
+//    @BeforeMethod
+//    public void before() {
+//        driver = new ChromeDriver();
+//    }
+//
+//    @AfterMethod
+//    public void tearDown() {
+//        driver.close();
+//        driver.quit();
+//    }
+//
+//    @Test
+//    public void chromeTest() {
+//        driver.get(GOOGLE_URL);
+//
+//        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
+//        acceptButton.click();
+//
+//        WebElement searchField = driver.findElement(By.name("q"));
+//        searchField.sendKeys("acodemy");
+//        searchField.sendKeys(Keys.ENTER);
+//
 //        Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
-    }
-}
+//    }
+//
+//    @Test(enabled = false)
+//    public void chromeTest2() {
+//        driver.get(GOOGLE_URL);
+//
+//        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
+//        acceptButton.click();
+//
+//        WebElement searchField = driver.findElement(By.name("q"));
+//        searchField.sendKeys("acodemy");
+//        searchField.sendKeys(Keys.ENTER);
+//
+////        Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
+//    }
+//}
